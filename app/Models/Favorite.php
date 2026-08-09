d940b36c2931bd0878f6f97fcfa1bb9861145bbf
Yuki Matsunaga <yuki1959yuki@gmail.com>
2026-08-09 14:37:41 +0900

    コンフリクトを解消

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -10,19 +10,16 @@ class Favorite extends Model
 {
     use HasFactory;
 
-    
     protected $fillable = [
         'user_id',
-        'book_id', 
+        'book_id',
     ];
 
-  
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    
     public function book(): BelongsTo
     {
         return $this->belongsTo(Book::class);
